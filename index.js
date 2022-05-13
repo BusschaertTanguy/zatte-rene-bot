bed202f67ab867d19615636c3e7cf78be437f161
Tanguy Busschaert <tanguy.busschaert.95@gmail.com>
2022-05-13 13:37:06 +0200

    cleanup index.js

--- index.js
+++ index.js
@@ -2,27 +2,16 @@ const { Client, Intents } = require('discord.js');
 const dotenv = require('dotenv');
 const path = require('path');
 const fs = require('fs');
-const {
-	joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus,
-} = require('@discordjs/voice');
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus } = require('@discordjs/voice');
 
-dotenv.config();
-
-const environment = process.env.NODE_ENV;
-
-if (environment !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
 	dotenv.config({ override: true });
 }
 
-const token = process.env.DISCORD_TOKEN;
 const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES];
 
 const client = new Client({
 	intents,
-});
-
-client.once('ready', () => {
-	console.log('Bot is ready');
 });
 
 client.on('messageCreate', ({ content, guild, member }) => {
@@ -35,7 +24,6 @@ client.on('messageCreate', ({ content, guild, member }) => {
 	const adapterCreator = guild.voiceAdapterCreator;
 
 	if (!channelId) {
-		console.log('Member should join a channel.');
 		return;
 	}
 
@@ -43,7 +31,6 @@ client.on('messageCreate', ({ content, guild, member }) => {
 
 	fs.readdir(soundDirectory, (error, files) => {
 		if (error) {
-			console.log(error.message);
 			return;
 		}
 
@@ -52,16 +39,13 @@ client.on('messageCreate', ({ content, guild, member }) => {
 		const randomFile = files[randomFileIndex];
 
 		if (!randomFile) {
-			console.log(`No file found with index: ${randomFileIndex}`);
 			return;
 		}
 
 		const randomFilePath = path.join(soundDirectory, randomFile);
-
-		console.log(`File: ${randomFilePath} selected`);
-
 		const resource = createAudioResource(randomFilePath);
 		const audioPlayer = createAudioPlayer();
+
 		const connection = joinVoiceChannel({
 			guildId,
 			channelId,
@@ -71,9 +55,7 @@ client.on('messageCreate', ({ content, guild, member }) => {
 		const subscription = connection.subscribe(audioPlayer);
 
 		audioPlayer.on('stateChange', ((oldState, newState) => {
-			console.log(`audio player state change: OLD: ${oldState.status}, NEW: ${newState.status}`);
 			if (oldState.status === AudioPlayerStatus.Playing && newState.status === AudioPlayerStatus.Idle) {
-				console.log('leaving channel');
 				audioPlayer.stop();
 				subscription.unsubscribe();
 				connection.destroy();
@@ -84,4 +66,4 @@ client.on('messageCreate', ({ content, guild, member }) => {
 	});
 });
 
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
